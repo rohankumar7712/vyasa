4884b8d30f63c8a9eefb625a338580606719362a
Rohankumar kanojiya <kanojiyarohankumar@gmail.com>
2026-08-18 10:41:31 +0530

    fix isuee

--- frontend/admin/fix.ps1
+++
@@ -1,2 +0,0 @@
-$content = Get-Content 'd:\Augmetik Software\Vyasa\frontend\admin\announcements.html'
-$content[160..($content.Length - 1)] | Set-Content 'd:\Augmetik Software\Vyasa\frontend\admin\announcements.html'
